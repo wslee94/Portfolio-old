dbcba270faaf5655ad5233228cacddb402f83db8
wslee3 <leemuo123@gmail.com>
2022-03-05 00:56:10 +0900

    [docs] 모바일앱 서버 프로젝트 추가

--- js/photoviewer.js
+++ js/photoviewer.js
@@ -18,6 +18,26 @@ $('[data-gallery=cloudlaw]').click(function (e) {
   new PhotoViewer(items, options);
 });
 
+$('[data-gallery=swagger]').click(function (e) {
+  e.preventDefault();
+
+  var items = [],
+    // get index of element clicked
+    options = {
+      index: $(this).index(),
+    };
+
+  // looping to create images array
+  $('[data-gallery=swagger]').each(function () {
+    let src = $(this).attr('href');
+    items.push({
+      src: src,
+    });
+  });
+
+  new PhotoViewer(items, options);
+});
+
 $('[data-gallery=lawai]').click(function (e) {
   e.preventDefault();
 
